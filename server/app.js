59a975cf426f859b300061ec0c74a94f241c5f4a
김현지 <khg0343@postech.ac.kr>
2023-05-14 01:06:55 +0900

    Library Setting

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 const { MONGO_URI } = config;
+
+app.use(hpp());
+app.use(helmet());
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+app.use(express.json());
 
 mongoose
   .connect(MONGO_URI, {
