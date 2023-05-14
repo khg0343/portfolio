499041662c5e3f42dae1a1730295e5ab09967595
김현지 <khg0343@postech.ac.kr>
2023-05-14 14:33:59 +0900

    Make Footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const totalView = 1;
+  const todayView = 2;
+
+  return (
+    <div id="main-footer" className="text-center p-2">
+      <Row>
+        <Col>
+          <p>김 현 지 Hyeonji Kim</p>
+          <p>
+            todayView : {todayView} | totalView : {totalView}
+          </p>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Footer;
