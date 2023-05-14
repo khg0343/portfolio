7ef44757adc99a6016a6fd393fa8a7344422de45
김현지 <khg0343@postech.ac.kr>
2023-05-14 23:00:18 +0900

    Fix AppNavbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,38 +1,21 @@
-import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
-import {
-  Navbar,
-  Container,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-  Row,
-  Col,
-} from "reactstrap";
+import React, { Fragment } from "react";
+import LoginModal from "../components/auth/LoginModal";
+import { Navbar, Container, Nav, Row, Col } from "reactstrap";
 
 const AppNavbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
     <Fragment>
       <Navbar bg="light" className="sticky-top">
         <Container>
           <Row>
             <Col>
-              <NavbarToggler onClick={toggle} />
-            </Col>
-            <Col>
-              {/* <Collapse isOpen={isOpen} navbar>
-                <Nav className="ml-auto d-flex justify-content-around" navbar>
-                  <NavItem>
-                    <Link to="/cart" className="nav-link">
-                      <i className="fas fa-shopping-cart"></i> Cart
-                    </Link>
-                  </NavItem>
-                </Nav>
-              </Collapse> */}
+              <Nav className="ml-auto d-felx justify-content-around" navbar>
+                {false ? (
+                  <h1 className="text-white">authLink</h1>
+                ) : (
+                  <LoginModal />
+                )}
+              </Nav>
             </Col>
           </Row>
         </Container>
