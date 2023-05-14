b78b4d0db4b60188c2182c404d97202d6886adda
김현지 <khg0343@postech.ac.kr>
2023-05-14 16:23:44 +0900

    Make Header

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/custom.scss";
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Header />
-        <Routes></Routes>
+        <h1>Hello Body</h1>
+        {/* <Routes></Routes> */}
         <Footer />
       </BrowserRouter>
     </Provider>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
+
+import { Button, IconButton } from "@mui/material";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const Header = () => {
-  return <div>Header</div>;
+  const lastEditDate = 16;
+  return (
+    <div id="page-header" className="mb-3">
+      <Row>
+        {/* <Col>icon</Col> */}
+        <Col className="m-2" xs="auto">
+          <Row clasName="m-2">
+            <Button variant="" startIcon={<AccountBoxIcon />}>
+              김 현 지 Hyeonji Kim
+            </Button>
+          </Row>
+        </Col>
+        <Col></Col>
+        <Col className="m-2" xs="auto">
+          <Row>
+            <Col className="header-textbtn">
+              <Button variant="text">{lastEditDate}시간 전 편집</Button>
+            </Col>
+            <Col className="header-textbtn">
+              <Button variant="text">공유</Button>
+            </Col>
+            <Col className="header-iconbtn">
+              <IconButton aria-label="comment" size="small">
+                <CommentOutlinedIcon />
+              </IconButton>
+            </Col>
+            <Col className="header-iconbtn">
+              <IconButton aria-label="all update" size="small">
+                <AccessTimeIcon />
+              </IconButton>
+            </Col>
+            <Col className="header-iconbtn">
+              <IconButton aria-label="favorite" size="small">
+                <StarBorderIcon />
+              </IconButton>
+            </Col>
+            <Col className="header-iconbtn">
+              <IconButton aria-label="more" size="small">
+                <MoreHorizIcon />
+              </IconButton>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Header;
